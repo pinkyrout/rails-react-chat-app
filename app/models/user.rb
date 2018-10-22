995fdd0d3471541865dd6b7f56a10e4b13003e24
pinkyrout <pinky@joshsoftware.com>
2018-10-22 16:02:51 +0530

    modified fetch_messages for current user itself

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :sent_messages, :class_name => 'Message', :foreign_key => 'from_id'
 
   def get_messages(from_user_id)
-    (sent_messages.where(to_id: from_user_id) + received_messages.where(from_id: from_user_id)).sort_by {|m| m[:created_at]}
+  	if(from_user_id == self.id)
+  		sent_messages.where(to_id: from_user_id).sort_by {|m| m[:created_at]}
+    else
+      (sent_messages.where(to_id: from_user_id) + received_messages.where(from_id: from_user_id)).sort_by {|m| m[:created_at]}
   end
 end
